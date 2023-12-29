dc173864d96ef8bacb19a412e0f6ff6a150c12b6
Quan Vo <tranminhquanvo@gmail.com>
2023-12-29 12:26:54 +0700

    Initialize the fraud-service module

---
+++ fraud-service/src/main/java/com/gonion/fraud/FraudServiceApplication.java
@@ -0,0 +1,11 @@
+package com.gonion.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FraudServiceApplication.class);
+  }
+}
