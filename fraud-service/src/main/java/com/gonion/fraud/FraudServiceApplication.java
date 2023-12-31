932c10e11b4c3c61db83d71ce3a3ef012d8ee451
Quan Vo <tranminhquanvo@gmail.com>
2023-12-31 23:35:16 +0700

    Register customer and fraud services with eureka server

--- fraud-service/src/main/java/com/gonion/fraud/FraudServiceApplication.java
+++ fraud-service/src/main/java/com/gonion/fraud/FraudServiceApplication.java
@@ -2,8 +2,10 @@ package com.gonion.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FraudServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(FraudServiceApplication.class, args);
