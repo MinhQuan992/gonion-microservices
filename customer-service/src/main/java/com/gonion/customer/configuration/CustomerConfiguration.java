17aed7c8061c775339e3ad9950128b976a5742c8
Quan Vo <tranminhquanvo@gmail.com>
2024-01-01 22:15:00 +0700

    Set up Load Balancer

--- customer-service/src/main/java/com/gonion/customer/configuration/CustomerConfiguration.java
+++ customer-service/src/main/java/com/gonion/customer/configuration/CustomerConfiguration.java
@@ -1,5 +1,6 @@
 package com.gonion.customer.configuration;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class CustomerConfiguration {
   @Bean
+  @LoadBalanced
   public RestTemplate restTemplate() {
     return new RestTemplate();
   }
